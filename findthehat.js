9b97ebed5894509b2309a006b58a85a5ae7689b7
Jason Smith <jason.smith.code@gmail.com>
2021-10-16 16:54:10 +0100

    completed

--- findthehat.js
+++ findthehat.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 const chalk = require('chalk');
-//const clear = require('clear-screen');
+const clear = require('clear-screen');
 
 const hat = '^';
 const hole = 'O';
@@ -9,8 +9,13 @@ const pathCharacter = '*';
 
 class Field {
     constructor(field){
-        this._field = field;    
+        this._field = field
+        this.x = 0;
+        this.y = 0;
+        this.plannedX = 0;
+        this.plannedY = 0;
     }
+
 
     start() {
         console.log(chalk.yellow('Welcome to the "Find Your Hat Game"'));
@@ -26,48 +31,69 @@ class Field {
         console.log(`The size of the grid is: ${answerRows} x ${answerWidth} and it contains ${answerHoles} holes`)
         // generate the field with the settings
         const generatedField = new Field(Field.generateField(answerWidth,answerRows,answerHoles)); // dependant on option questions
-        // print the field
-        generatedField.print();
-        // ask user which direction to take
-        //generatedField.startPos()
+
         let playing = true;
 
         while (playing) {
-            this.playerMoves()
+            const maxHeight = answerRows;
+            const maxWidth = answerWidth;
+            
+            clear()
             generatedField.print();
-            // define current position
-            // PROBLEM STARTS HERE
-            // Trying to select a grid position
-            console.log(this.testPosition)
-            console.log(this.field[0][0]); // type error
-            console.log(this._field[0][0]); // type error
-            console.log(generatedField) // prints the class not the 2d array
-            console.log(this._field) // undefined
-            console.log(this.locationOne) // undefined
-   
+            //initiate the movement input
+            let movePlayer = prompt(chalk.red('W = up, A = left, S = down, D = right: '));
+            switch(movePlayer.toLowerCase()) {
+                
+                case 'w':
+                    this.plannedY -= 1;
+                    if (this.plannedY < 0 ){
+                        this.plannedY = 0;  
+                        prompt(chalk.red('You cant move any further up - Press any key continue'));
+                    }
+                    break;
+                case 's':
+                    this.plannedY += 1;
+                    if (this.plannedY > (maxHeight - 1) ){
+                        this.plannedY = (maxHeight - 1);  
+                        prompt(chalk.red('You cant move any further down - Press any key continue'));
+                    }
+                    break;
+                case 'a':
+                    this.plannedX -= 1;
+                    if (this.plannedX < 0 ){
+                        this.plannedX = 0;
+                        prompt(chalk.red('You cant move any further left - Press any key continue'));  
+                    }
+                    break;
+                case 'd':
+                    this.plannedX += 1;
+                    
+                    if (this.plannedX > (maxWidth -1)){
+                        this.plannedX = (maxWidth -1);  
+                        prompt(chalk.red('You cant move any further Right - Press any key continue'));
+                    }
+                    break;
+                default:
+                    break;
+            }    
+            // over write the array element to show where play has been
+            generatedField._field[this.y][this.x] = pathCharacter;
 
-            playing = false;
-            // try to print the following
-            // console.log(north of your current position is)   Should be "offGrid"
-            // console.log(east of your current position is)    Should be either fieldCharacter or hole
-            // console.log(south of your current position is)   Should be either fieldCharacter or hole
-            // console.log(west of your current position is)    Should be "offGrid"
-            /*
-            if (nextPos === hole) {
-                console.log("You just fell down a hole and died, game over");
+            let pendingPlayerPosition = generatedField._field[this.plannedY][this.plannedX];
+
+            // if the player move into a hole end the game
+            if (pendingPlayerPosition === hole) {
+                console.log("You fell in a hole and died");
                 playing = false;
             }
 
-            if (nextPos === outsideGrid) {
-                console.log("You just fell off the grid and died, game over");
+            // if the player finds the hat end the game
+            if (pendingPlayerPosition === hat) {
+                console.log("Congratulations you found the hat");
                 playing = false;
             }
 
-            if (nextPos === hatPos) {
-                console.log("Congratulations, you just won the game");
-                playing = false;
-            }
-            */
+            generatedField._field[this.plannedY][this.plannedX] = pathCharacter;
         }
     }
 
@@ -88,12 +114,12 @@ class Field {
     questionWidth() {
         let widthQuestionAnswered = false;
         while (!widthQuestionAnswered) {
-            let userRowWidth = Number(prompt(chalk.red('How many Rows would you like in your grid, pick a number from 3 to 5: ')));
-            if (userRowWidth >= 3 && userRowWidth <= 5) {;
+            let userRowWidth = Number(prompt(chalk.red('How many Rows would you like in your grid, pick a number from 3 to 10: ')));
+            if (userRowWidth >= 3 && userRowWidth <= 10) {;
                 widthQuestionAnswered = true;
                 return userRowWidth;
             } 
-            if (userRowWidth < 3 || userRowWidth > 5) {
+            if (userRowWidth < 3 || userRowWidth > 10) {
                 console.log("Incorrect entry")
             }      
         }
@@ -119,30 +145,13 @@ class Field {
         }
     }
 
-    playerMoves() {
-
-        let movePlayer = prompt(chalk.red('W = up, A = left, S = down, D = right: '));
-        if (movePlayer === "W") {
-            //this.verticalPos -= 1;
-            //console.log(`Your current position is ${this.currentPos}`)
-        } else if (movePlayer === "A") {
-            //this.horizontalPos -= 1;
-            //console.log(`Your current position is ${this.currentPos}`)
-        } else if (movePlayer === "S") {
-            //this.verticalPos += 1;
-            //console.log(`Your current position is ${this.currentPos}`)
-        } else if (movePlayer === "D") {
-            //this.horizontalPos += 1;
-            //console.log(`Your current position is ${this.currentPos}`)
-        }   
-    }
-
     static generateField(fieldWidth, fieldHeight, holesAmount) {
         // create blank field containing field characters
-        var field = new Array(fieldHeight).fill(fieldCharacter).map( () => new Array(fieldWidth).fill(fieldCharacter));
+        const field = new Array(fieldHeight).fill(fieldCharacter).map( () => new Array(fieldWidth).fill(fieldCharacter));
 
         field[0][0] = pathCharacter;
         // place hat in bottom right
+        const hatPos = field[fieldHeight-1][fieldWidth-1]
         field[fieldHeight-1][fieldWidth-1] = hat;
         for (let i = 0; i < holesAmount; i++) {
             // cycle through 2d array, and randomly select numbers to change the value of the item inside the array  
@@ -163,3 +172,4 @@ class Field {
 let newGame = new Field;
 
 newGame.start()
+
